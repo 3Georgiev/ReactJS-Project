2a4e5b176841da9287f4d53baac238f9efdc8e76
Georgi GeorgIev <joretivac@gmail.com>
2023-12-05 19:47:59 +0200

    added scrolltotop on change

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/scrolltotop/ScrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
