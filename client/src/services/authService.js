cdb7ff9dd0a71262c94b211616aca292b4fcf2e8
Georgi GeorgIev <joretivac@gmail.com>
2023-12-05 19:22:19 +0200

    authService register

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -10,6 +10,11 @@ export const login = (email, password) => {
   return result;
 };
 
+export const register = (email, password) => {
+  const result = request.post(`${baseUrl}/register`);
+  return result;
+};
+
 export const logout = () => {
   const result = request.get(`${baseUrl}/logout`);
   return result;
