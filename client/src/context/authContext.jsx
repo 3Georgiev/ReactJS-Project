e5587673b78809e2404689cd81ebfdf2190ad3ba
Georgi GeorgIev <joretivac@gmail.com>
2023-12-05 18:14:45 +0200

    authContext update

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -1,16 +1,32 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import * as authService from "../services/authService";
+import { useNavigate } from "react-router-dom";
+import Path from "../paths";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({});
+
+  const navigate = useNavigate();
+
   const loginSubmitHandler = async (values) => {
     const result = await authService.login(values.email, values.password);
+
+    setAuth(result);
+
+    localStorage.setItem("accessToken", result.accessToken);
+
+    navigate(Path.Home);
+
     console.log(result);
   };
 
   const values = {
     loginSubmitHandler,
+    email: auth.email,
+    userId: auth._id,
+    isAuthenticated: !!auth.email,
   };
 
   return (
